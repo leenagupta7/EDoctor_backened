85a346d545b237cab90f831719b9ab4c7f286c93
bemybunny <bemybunny123@gmail.com>
2024-06-29 21:09:07 +0530

    add url

--- Socket.js
+++ Socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"],
+        origin: ["https://edoctor-eight.vercel.app/"],
         methods: ["GET", "POST"]
     }
 });
